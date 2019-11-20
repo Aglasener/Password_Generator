9f96a9dcbd4480468f7d3ec2ea61609f14876e35
Andres Glasener <52112240+Aglasener@users.noreply.github.com>
2019-11-20 18:11:47 -0500

    moved script to html file

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-var passArray = [];
-var charToUse = [];
-var specArray = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","_","~"];
-var numArray = ["1","2","3","4","5","6","7","8","9","0"];
-var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowcaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-
-
 function lengthSelect() {
     var passLength = prompt("Enter your desired password length (must be between 8 and 128 characters)");
     if (passLength >= 8 && passLength <= 128){
@@ -46,3 +37,33 @@ function charSelect() {
             charSelect();
         }
 }
+
+var passArray = [];
+var charToUse = [];
+var specArray = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","_","~"];
+var numArray = ["1","2","3","4","5","6","7","8","9","0"];
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowcaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+var passLength = lengthSelect();
+charSelect();
+
+console.log(charToUse);
+
+for(var i = 0; i < passLength; i++){
+    var x = Math.floor(Math.random() *  charToUse.length);
+    var y = Math.floor(Math.random() * charToUse[x].length);
+    var char = charToUse[x][y];
+    passArray.push(char);
+}
+
+console.log(passArray);
+
+var password = "";
+for (var i = 0; i < passArray.length; i++){
+    password = password + passArray[i];
+}
+
+console.log(password);
+alert("Your password is: " + password);
