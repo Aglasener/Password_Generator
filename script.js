396cb529295d1f35bdef6526d80e838d06ab1d2f
Andres Glasener <52112240+Aglasener@users.noreply.github.com>
2019-11-20 16:50:34 -0500

    Defined variables called for by functions

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var passArray = [];
+var charToUse = [];
+var specArray = ["!","#","$","%","&","(",")","*","+","-",".","/",":",";","<","=",">","?","@","^","_","~"];
+var numArray = ["1","2","3","4","5","6","7","8","9","0"];
+var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowcaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+
 function lengthSelect() {
     var passLength = prompt("Enter your desired password length (must be between 8 and 128 characters)");
     if (passLength >= 8 && passLength <= 128){
