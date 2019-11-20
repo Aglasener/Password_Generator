f5981408a1ec7576460ded016446764fcdd0eaae
Andres Glasener <52112240+Aglasener@users.noreply.github.com>
2019-11-20 16:47:36 -0500

    Added function for user to select character options

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ function lengthSelect() {
     return passLength;        
 }
 
+function charSelect() {
+    var specChar = confirm("Do you want to use special characters?");
+        if (specChar) {
+            alert("You have chosen to use special characters!");
+            charToUse.push(specArray);
+        } 
+    var numChar = confirm("Do you want to use numbers?");
+        if (numChar) {
+            alert("You have chosen to use numeric characters!");
+            charToUse.push(numArray);
+        }
+    var lowerChar = confirm("Do you want to use lowercase characters?");
+        if (lowerChar) {
+            alert("You have chosen to use lowercase characters!")
+            charToUse.push(lowcaseArray);
+        }     
+    var upperChar = confirm("Do you want to use uppercase characters?");
+        if (upperChar) {
+            alert("You have chosen to use uppercase characters!");
+            charToUse.push(uppercaseArray);
+        }
+        else if (specChar == false && numChar == false && lowerChar == false && upperChar == false){
+            alert("You must choose a character type!");
+            charSelect();
+        }
+}
